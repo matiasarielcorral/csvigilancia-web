1fe301a02fa83dc6c8c314c3837c05c647e1d5e4
matiasariel.corral <matiasariel.corral@gmail.com>
2025-09-03 01:05:56 -0400

    eliminar código del menú hamburguesa para simplificar la carga del encabezado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,5 @@
 // Menú Hamburguesa (Responsive)
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
 
-    hamburger.addEventListener('click', () => {
-        navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
-    });
-
-    // Cerrar menú al hacer clic en un link (opcional)
-    document.querySelectorAll('.nav-links a').forEach(link => {
-        link.addEventListener('click', () => {
-            if (window.innerWidth <= 768) {
-                navLinks.style.display = 'none';
-            }
-        });
-    });
-});
 // Cargar el header en las páginas
  fetch('header.html')
           .then(res => res.text())
